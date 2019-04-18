80294680eb7d59b50f23fb991873cb0ea35bac4a
AlexCitul <39662554+AlexCitul@users.noreply.github.com>
2019-04-18 03:49:31 -0500

    Add files via upload

--- Country.py
+++ Country.py
@@ -2,29 +2,35 @@ import cgi
 import requests
 import urllib.request
 import json
+class Country():
 
-print()
-#funcion cgi.FieldStorage para que lea los datos ingresados desde la URL
-barra_Direcciones = cgi.FieldStorage()
-#getvalue para obtener el valor de la URL, se le puse 'name' para que se escriba de la sig manera name=mexico
-valor_Barra = barra_Direcciones.getvalue('name')
-#URL que es consumido
-url = "https://restcountries.eu/rest/v2/name/"
-if 'name' not in barra_Direcciones:
-	print('''<html>
-	<head><title>Countries</title></head>
-	<body>
-	<h1>No has ingresado ningun valor</h1>
-	</body>
-	</html>''')
-else:
-	#se le concatena la url al valor de barra para que este complemente la URL 
-	agregando = url+valor_Barra
-	# se abre el URL con la funcion open
-	urll = urllib.request.urlopen(agregando)
-	countryData = urll.read()
-	theJSON = json.loads(countryData)
-	#para que se vea de manera correcta el JSON
-	json_str = json.dumps(theJSON,sort_keys=True, indent=4)
-	#se imprime el resultado
-	print(json_str)
+	def __init__(self,url):
+		print()
+		barra_Direcciones = cgi.FieldStorage()
+		valor_Barra = barra_Direcciones.getvalue('name')
+		self.url = url
+		if 'name' not in barra_Direcciones:
+			print('''<html>
+			<head><title>Countries</title></head>
+			<body>
+			<h1>No has ingresado ningun valor</h1>
+			</body>
+			</html>''')
+		else:
+			#se le concatena la url al valor de barra para que este complemente la URL 
+			agregando = self.url+valor_Barra
+			# se abre el URL con la funcion open
+			urll = urllib.request.urlopen(agregando)
+			countryData = urll.read()
+			theJSON = json.loads(countryData)
+			#para que se vea de manera correcta el JSON
+			json_str = json.dumps(theJSON,sort_keys=True, indent=4)
+			#se imprime el resultado
+			print(json_str)
+			
+if __name__ == '__main__':
+	link = Country("https://restcountries.eu/rest/v2/name/")
+	
+
+
+
--- servidor.py
+++ servidor.py
@@ -2,22 +2,28 @@
 Alexander de Jesus Citul Puc
 '''
 from http.server import HTTPServer, CGIHTTPRequestHandler
-# Solicitudes HTTP
 class servidor(CGIHTTPRequestHandler):
-  
-  cgi_directories = ["/"]
-      
-def run():
-  print('Servidor ejecutado...')
-  # configuración del servidor
-  # Nombre y puerto para iniciar el servidor
-  server_address = ('localhost', 8080)
-  httpd = HTTPServer(server_address, servidor)
-  print('Corriendo servidor...')
-  #Try para que cuando se aprete ctrl c el servidor se termine y muestre el mensaje
-  try:
-      httpd.serve_forever()
-  except KeyboardInterrupt:
-        print('KeyboardInterrupt Ctrl + C')
- 
-run()
+	cgi_directories = ["/"]
+
+	def handle_http(self,status,content_type,content):
+
+		self.send_response(status)
+		self.send_header('Content-type',content_type)
+		self.end_headers()
+
+		return bytes(content,'UTF-8')
+
+	def respond(self,result):
+	    content = self.handle_http(200, 'application/json',result)
+	    self.wfile.write(content)  
+
+if __name__ == '__main__':
+	print('Servidor ejecutado...')
+	server_address = ('localhost', 8080)
+	httpd = HTTPServer(server_address, servidor)
+	print('Corriendo servidor...')
+	#Try para que cuando se aprete ctrl c el servidor se termine y muestre el mensaje
+	try:
+	  httpd.serve_forever()
+	except KeyboardInterrupt:
+	    print('KeyboardInterrupt Ctrl + C')
